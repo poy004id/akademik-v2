3eb1a6dd05e101ade8a58207ef10a0f694464afb
poy004id <agungsupoyo@gmail.com>
2023-04-27 09:07:02 +0700

    update files name

--- src/navigations/appNavigator/stacks/LogbookStack.js
+++ src/navigations/appNavigator/stacks/LogbookStack.js
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StackHeader from '../../components/StackHeader';
-import Logbook from '../../../screens/logbookStack';
+import Logbook from '../../../screens/LogbookStack';
 
 function LogbookNavigator() {
 const Stack = createNativeStackNavigator();
